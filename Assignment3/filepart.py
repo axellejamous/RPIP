5dbfed8e1fc969add3dbd2a6fba1d0e6fb8a0150
Axelle Jamous <axelle.jamous@student.ap.be>
2017-05-07 22:48:37 +0200

    Test n2

--- Assignment3/filepart.py
+++ Assignment3/filepart.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
-from time import sleep
-#from gpiozero import Button, LED
+from time import sleep, strftime
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(11, GPIO.IN)         #Read output from IR motion sensor
+GPIO.setup(2, GPIO.IN)          #Button
 GPIO.setup(3, GPIO.OUT)         #LED output pin
 
-#button = Button(3)
-#led = LED(2)
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+globFlag = 0
 
 def cleanupTool():
     lineCln()
@@ -16,47 +17,59 @@ def cleanupTool():
     return
 
 def lineCln():
-    lines = readFile()
+    lines = readFile("logFile.txt")
     
     #print every line and ask user if he wants to delete it
     
     return
 
 def rangeCln():
-    lines = readFile()
+    lines = readFile("logFile.txt")
 
     #ask user for start and stop date
     #delete all entries in file in given date range
 
     return
 
-def readFile():
+def readFile(fileName):
     #read file line per line w timestamps
-    f = open(logs, r)
+    f = open(os.path.join(__location__, fileName), "r")
     lines = f.readlines() #list
     f.close()
     return lines
 
+def writeFile(fileName, stringToFile):
+    f = open(os.path.join(__location__, fileName), "a")
+    f.write(stringToFile)
+    f.close
+
 def main():
+    global globFlag
+    
     i=GPIO.input(11)
     if i==0:                 #When output from motion sensor is LOW
+        #set flag back to 0 for time
+        globFlag = 0
+        
         print "No intruders",i
         GPIO.output(3, 0)  #Turn OFF LED
-        #led.on()
         sleep(0.1)
     elif i==1:               #When output from motion sensor is HIGH
+        if globFlag==0:
+            #output time to file
+            writeFile("timeFile.txt", time.strftime("%a, %d %b %Y %H:%M:%S"))
+            #set flag to on
+            globFlag = 1
+        else:
+            #door is still open but hasn't been shut
+            
         print "Intruder detected",i
         GPIO.output(3, 1)  #Turn ON LED
-        #led.off()
         sleep(0.1)
-    
-    #try:
-    #    break
-    #except ValueError:
-    #    print "Oops!  That was no valid number.  Try again..."
-    #return
 
-#toplevel script will only execute if ran directly:
+
+#toplevel script
+#below will only execute if ran directly - above is always accessible 
 if __name__ == '__main__':
     while True:
         main()
