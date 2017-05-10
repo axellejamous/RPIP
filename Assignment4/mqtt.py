c751dd7e01b4e33ad0a49d2901b7fa7792babef7
Axelle <axelle.jamous@student.ap.be>
2017-05-10 23:54:51 +0200

    Added code not tested yet

--- Assignment4/mqtt.py
+++ Assignment4/mqtt.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import os
 from time import sleep
+import paho.mqtt.client as mqtt
 
 btn1 = 5 #red
 btn2 = 3 #yellow
@@ -16,8 +17,61 @@ GPIO.setup(btnMaster, GPIO.IN)
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 
+# aanmaken van mqtt client object
+mqttc = mqtt.Client()
+
+# toewijzen van callback functies
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+#connect and subscribe
+mqttc.connect("127.0.0.1")
+mqttc.subscribe("home/groundfloor/livingroom/lights/lightx")
+
+def set_leds(leds, states):
+    io.output(leds, states)
+
+# callback functie voor connect event
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: "+str(rc))
+
+# callback functie voor message event
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    try:
+        # payload omzetten van bytestring naar string
+        p = msg.payload.decode("utf-8")
+
+        # json wordt verwacht json string moet omgezet worden naar een python
+        #  dictonary voor verwerking
+        x = json.loads(p)
+
+        #
+        set_leds(leds, tuple(x['leds']))
+        return
+    except Exception as e:
+        print(e)
+
+# callback functie voor publish  event
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+# callback functie voor subscribe event
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def main():
+    try:
+        while True:
+            mqttc.loop()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        io.cleanup()
+
 #toplevel script
 #below will only execute if ran directly - above is always accessible
 if __name__ == '__main__':
-    while True:
-        main()
+    main()
