fa1b59e8cf1473aa5708396f78cdcfcfbc90cebd
Axelle <axelle.jamous@student.ap.be>
2017-05-13 02:33:01 +0200

    Changed callback for error Param must be callable

--- Assignment4/mqtt.py
+++ Assignment4/mqtt.py
@@ -88,9 +88,9 @@ def snd_msg(led):
     dataToSend=json.dumps({"leds":[led1State,led2State]})
     mqttc.publish(snd_topic, dataToSend)
 
-io.add_event_detect(btn1,io.FALLING,callback=snd_msg(1),bouncetime=500)
-io.add_event_detect(btn2,io.FALLING,callback=snd_msg(2),bouncetime=500)
-io.add_event_detect(btnMaster,io.FALLING,callback=snd_msg(3),bouncetime=500)
+io.add_event_detect(btn1,io.FALLING,callback=lambda *a: snd_msg(1),bouncetime=500)
+io.add_event_detect(btn2,io.FALLING,callback=lambda *a: snd_msg(2),bouncetime=500)
+io.add_event_detect(btnMaster,io.FALLING,callback=lambda *a: snd_msg(3),bouncetime=500)
 
 ############### main ##################
 
