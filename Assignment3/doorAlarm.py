1837fd2cffbf70f2dcbbd749af9385d364bb2f95
Axelle <axelle.jamous@student.ap.be>
2017-05-24 00:55:06 +0200

    raw_input > input

--- Assignment3/doorAlarm.py
+++ Assignment3/doorAlarm.py
@@ -29,17 +29,17 @@ def cleanupTool():
     lines = readFile("timeFile.txt")
 
     #Offer user two modes in which he can cleanup the file
-    mode=raw_input("Type 1 to clean up line per line, type 2 to clean up a certain range: ")
+    mode=input("Type 1 to clean up line per line, type 2 to clean up a certain range: ")
     if(mode=="1"):
         print("Cleaning line per line")
         for line in list(lines):
-            rm=raw_input("Do you want to remove this line? (y)")
+            rm=input("Do you want to remove this line? (y)")
             if(rm=="y"):
                 lines.remove(line)
     elif(mode=="2"):
         print("".join(lines))
-        startdate=time.strptime(raw_input("Select start date to remove"),"%a, %d %b %Y %H:%M:%S")
-        enddate=time.strptime(raw_input("Select end date to remove"),"%a, %d %b %Y %H:%M:%S")
+        startdate=time.strptime(input("Select start date to remove"),"%a, %d %b %Y %H:%M:%S")
+        enddate=time.strptime(input("Select end date to remove"),"%a, %d %b %Y %H:%M:%S")
         print("Start: " + startdate + " End: " + enddate)
         for line in list(lines):
             dateLine=strptime(line.rstrip("\n"),"%a, %d %b %Y %H:%M:%S")
