40b7d320114d705119aaf3b1bfdd6987ef7366bf
Axelle <axelle.jamous@student.ap.be>
2017-05-26 00:31:02 +0200

    added all btn functions

--- Assignment5/doorAlarm.py
+++ Assignment5/doorAlarm.py
@@ -2,20 +2,21 @@ import os
 from time import sleep, strftime, time, strptime
 from gpiozero import DistanceSensor, LED, Button
 
-###################declarations#######################
+######################setup#########################
+toggleBtn = Button(2)
+distanceBtn = Button(3)
+holdBtn = Button(4, hold_time=5)
+led = LED(14)
+ultrasonic = DistanceSensor(echo=17, trigger=18) #threshold is set to 0.3m standard
+# LED WAS 3
+# TRIGGER WAS 4
 
-button = Button(5, hold_time=5)
-led = LED(3)
-ultrasonic = DistanceSensor(echo=17, trigger=4) #threshold is set to 0.3m standard
-
+#################global declarations##################
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-#global
-fileFlag = btnState = 0
+fileFlag = holdState = 0
 ledState = False
 
 ####################functions#########################
-
 def readFile(fileName):
     #read file line per line w timestamps
     f = open(os.path.join(__location__, fileName), "r")
@@ -33,20 +34,41 @@ def writeFile(fileName, stringToFile):
     f.write(stringToFile)
     f.close()
 
-def timer(self):
-    global btnState
+def writeto_file():
+    #first time alarm starts going off, write to file:
+    if fileFlag==0:
+        #output time to file
+        appendFile("timeFile.txt", "{}\n".format(strftime("%a, %d %b %Y %H:%M:%S")))
+        #set flag to on
+        fileFlag = 1
 
+def alarmer():
+    if holdState == 0:
+        ledState = not ledState
+        led.value = ledState #turn on or off led depending on state
+
+def timer():
+    global holdState, ledState
     led.off
-    btnState = 1 #so that it doesn't get triggered in main anymore
+    ledState = False
+    holdState = 1 #so that it doesn't get triggered in main anymore
 
+def toggler():
+    global holdState
+    holdState = 0
+    alarmer()
+
+def showDistance():
+    print("distance: " + str(ultrasonic.distance))
 
 ###################interrupts#######################
-button.when_held = timer
-#button.when_pressed = timer
+holdBtn.when_held = timer
+toggleBtn.when_pressed = toggler
+distanceBtn.when_pressed = showDistance
 
 #####################main###########################
 def main():
-    global fileFlag, ledState, btnState
+    global fileFlag, ledState, holdState
 
     #alarm off:
     ultrasonic.wait_for_out_of_range()
@@ -54,23 +76,16 @@ def main():
 
         led.off #Turn OFF led
 
-        fileFlag = 0
+        fileFlag = holdState = 0 #reset button press so that the alarm goes off again & file write
         ledState = False #reset alarmled
-        btnState = 0 #reset button press so that the alarm goes off again
 
     #alarm on:
     ultrasonic.wait_for_in_range()
         print("Door open")
-        #first time alarm starts going off, write to file:
-        if fileFlag==0:
-            #output time to file
-            appendFile("timeFile.txt", "{}\n".format(strftime("%a, %d %b %Y %H:%M:%S")))
-            #set flag to on
-            fileFlag = 1
 
-        if btnState == 0:
-            btnState = not btnState
-            led.value = ledState #turn on or off led depending on state
+        writeto_file()
+        alarmer()
+
 
 #################toplevel script####################
 if __name__ == '__main__':
